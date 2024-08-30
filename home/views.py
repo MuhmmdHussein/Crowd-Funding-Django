77f056e599a3bc8b84bb227bb6f3924ea668dc3d
MuhmmdHussein <muhmmdamer@gmail.com>
2024-08-30 10:02:51 +0300

    #Handling Home

--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Project
 
-# Create your views here.
+def homepage(request):
+    context = {
+        'title': 'Welcome to My Website',
+        'message': 'This is a simple homepage.',
+        'items': ['Item 1', 'Item 2', 'Item 3']
+    }
+    return render(request, 'home/homepage.html', context)
 
-def index(request):
 
-    # Page from the theme 
-    return render(request, 'pages/index.html')
+from django.shortcuts import render
+
+def all_projects(request):
+     projects = Project.objects.all() 
+     context = {
+        'projects': projects,
+    }
+     return render(request, 'home/all_projects.html')
