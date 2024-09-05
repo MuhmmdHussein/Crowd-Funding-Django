5770531a7f9452ef17b83f91a0de27642874bc82
Esraa Adel <ekoryem@gmail.com>
2024-09-05 19:11:38 +0300

    save updated

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import redirect, render
 from authentication.models import Register
+from home.models import Project
 from profiles.forms import EditProfileForm
 
 # Create your views here.
 
 
 def profile (request):
-   
-    return render(request, 'profile/Profile.html')
+    if 'user_id'  in request.session :
+        try:
+            user_object= Register.objects.get(id = request.session['user_id'])
+            projects =Project.objects.filter(user_id =  request.session['user_id'] )
+            # donations = Donation.objects.filter(user_id =  request.session['user_id'] )
+            
+            images = []
+            for project in projects:
+                images.append(project.image_set.all().first().images.url)
+
+        except Register.DoesNotExist:
+            return redirect('/')
+        
+        
+        return render(request, 'profile/Profile.html', {"user" :user_object ,"projects":projects ,'images':images})
+    else:
+        return redirect("/" )
+    # return render(request, 'profile/Profile.html')
    
 
 
